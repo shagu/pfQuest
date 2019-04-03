fc862c10b0adf749b3cc1dda6163dc865db6f61f
Road-block <dridzt.addons@hotmail.com>
2019-04-03 13:47:39 +0300

    slashcmd: return from the command parser early
    * allow to pass uknown terms to the browser

--- slashcmd.lua
+++ slashcmd.lua
@@ -18,6 +18,7 @@ SlashCmdList["PFDB"] = function(input, editbox)
     DEFAULT_CHAT_FRAME:AddMessage("|cff33ffcc/db|cffffffff meta <relation> [min, [max]] |cffcccccc - " .. pfQuest_Loc["show related objects on the map"])
     DEFAULT_CHAT_FRAME:AddMessage("|cff33ffcc/db|cffffffff scan |cffcccccc - " .. pfQuest_Loc["scan the server for items"])
     DEFAULT_CHAT_FRAME:AddMessage("|cff33ffcc ->|cffffffff " .. pfQuest_Loc["Available relations"] .. ": |cff33ffccchests|r, |cff33ffccherbs|r, |cff33ffccmines|r")
+    return
   end
 
   local commandlist = { }
@@ -43,67 +44,88 @@ SlashCmdList["PFDB"] = function(input, editbox)
   if (arg1 == "item") then
     local maps = pfDatabase:SearchItem(arg2, meta, "LOWER")
     pfMap:ShowMapID(pfDatabase:GetBestMap(maps))
+    return
   end
 
   -- argument: vendor
   if (arg1 == "vendor") then
     local maps = pfDatabase:SearchVendor(arg2, meta, "LOWER")
     pfMap:ShowMapID(pfDatabase:GetBestMap(maps))
+    return
   end
 
   -- argument: unit
   if (arg1 == "unit") then
     local maps = pfDatabase:SearchMob(arg2, meta, "LOWER")
     pfMap:ShowMapID(pfDatabase:GetBestMap(maps))
+    return
   end
 
   -- argument: object
   if (arg1 == "object") then
     local maps = pfDatabase:SearchObject(arg2, meta, "LOWER")
     pfMap:ShowMapID(pfDatabase:GetBestMap(maps))
+    return
   end
 
   -- argument: quest
   if (arg1 == "quest") then
     local maps = pfDatabase:SearchQuest(arg2, meta, "LOWER")
     pfMap:ShowMapID(pfDatabase:GetBestMap(maps))
+    return
   end
 
   -- argument: quests
   if (arg1 == "quests") then
     local maps = pfDatabase:SearchQuests(meta)
     pfMap:UpdateNodes()
+    return
   end
 
   -- argument: meta
   if (arg1 == "meta") then
     local maps = pfDatabase:SearchMetaRelation({ commandlist[2], commandlist[3], commandlist[4] }, meta)
     pfMap:ShowMapID(pfDatabase:GetBestMap(maps))
+    return
   end
 
   -- argument: clean
   if (arg1 == "clean") then
     pfMap:DeleteNode("PFDB")
     pfMap:UpdateNodes()
+    return
   end
 
   -- argument: show
   if (arg1 == "show") then
     if pfBrowser then pfBrowser:Show() end
+    return
   end
 
   -- argument: show
   if (arg1 == "config") then
     if pfQuestConfig then pfQuestConfig:Show() end
+    return
   end
 
   -- argument: locale
   if (arg1 == "locale") then
     DEFAULT_CHAT_FRAME:AddMessage("|cff33ffccLocales|r:" .. pfDatabase.dbstring)
+    return
   end
 
   -- argument: scan
   if (arg1 == "scan") then
     pfDatabase:ScanServer()
+    return
+  end
+
+  -- argument: <text>
+  if (type(arg1)=="string") then
+    if pfBrowser then 
+      pfBrowser:Show()
+      pfBrowser.input:SetText(string.format("%s %s",arg1,arg2))
+    end
+    return
   end
 end
