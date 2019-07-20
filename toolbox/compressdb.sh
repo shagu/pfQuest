7a9d540adb5f5f05cb102cb346193f33972fc793
shagu <ericmauser@googlemail.com>
2019-07-20 22:08:04 +0200

    toolbox: add tbc databases to compression

--- toolbox/compressdb.sh
+++ toolbox/compressdb.sh
@@ -18,13 +18,15 @@ function compress() {
   mv /tmp/$(basename $1) $1
 }
 
-echo "===== compressing DB ====="
+echo "===== compressing DB (Vanilla) ====="
 echo "-> database"
 compress db/items.lua ws
+compress db/refloot.lua ws
 compress db/objects.lua ws
 compress db/units.lua ws
 compress db/quests.lua ws
 compress db/meta.lua ws
+compress db/minimap.lua ws
 
 echo "-> locales"
 for loc in db/*/; do
@@ -34,4 +36,24 @@ for loc in db/*/; do
   compress $loc/quests.lua nc
   compress $loc/units.lua
   compress $loc/zones.lua
+done
+
+echo "===== compressing DB (TBC) ====="
+echo "-> database"
+compress db/items-tbc.lua ws
+compress db/refloot-tbc.lua ws
+compress db/objects-tbc.lua ws
+compress db/units-tbc.lua ws
+compress db/quests-tbc.lua ws
+compress db/meta-tbc.lua ws
+compress db/minimap-tbc.lua ws
+
+echo "-> locales"
+for loc in db/*/; do
+  compress $loc/items-tbc.lua
+  compress $loc/objects-tbc.lua
+  compress $loc/professions-tbc.lua
+  compress $loc/quests-tbc.lua nc
+  compress $loc/units-tbc.lua
+  compress $loc/zones-tbc.lua
 done
