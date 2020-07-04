fa21e9777c247f9bcab354190ec55165130f2ba8
shagu <ericmauser@googlemail.com>
2020-07-04 21:56:18 +0200

    extractor: add levels to rare mobs

--- toolbox/extractor.lua
+++ toolbox/extractor.lua
@@ -978,12 +978,13 @@ for _, expansion in pairs(expansions) do
     do -- raremobs
       local creature_template = {}
       local query = mysql:execute([[
-        SELECT Entry, Rank FROM `creature_template` WHERE Rank = 4 OR Rank = 2
+        SELECT Entry, Rank, MinLevel FROM `creature_template` WHERE Rank = 4 OR Rank = 2
       ]])
 
       while query:fetch(creature_template, "a") do
-        local entry   = tonumber(creature_template.Entry)
-        pfDB["meta"..exp].rares[entry] = 0
+        local entry = tonumber(creature_template.Entry)
+        local level = tonumber(creature_template.MinLevel)
+        pfDB["meta"..exp].rares[entry] = level
       end
     end
 
