897e9e354607505e689cda1f087d181bfc65d085
shagu <ericmauser@googlemail.com>
2020-08-04 17:16:04 +0200

    route: separate arrow from route calculation

--- route.lua
+++ route.lua
@@ -76,23 +76,6 @@ local function DrawLine(path,x,y,nx,ny,hl)
 end
 
 pfQuest.route = CreateFrame("Frame", "pfQuestRoute", UIParent)
-pfQuest.route:SetPoint("CENTER", 0, -100)
-pfQuest.route:SetWidth(56)
-pfQuest.route:SetHeight(42)
-pfQuest.route:SetClampedToScreen(true)
-pfQuest.route:SetMovable(true)
-pfQuest.route:EnableMouse(true)
-pfQuest.route:RegisterForDrag('LeftButton')
-pfQuest.route:SetScript("OnDragStart", function()
-  if IsShiftKeyDown() then
-    this:StartMoving()
-  end
-end)
-
-pfQuest.route:SetScript("OnDragStop", function()
-  this:StopMovingOrSizing()
-end)
-
 pfQuest.route.firstnode = nil
 pfQuest.route.coords = {}
 
@@ -106,73 +89,10 @@ pfQuest.route.AddPoint = function(self, tbl)
   self.firstnode = nil
 end
 
-local speed = 0
-local lastdist = 0
-local speedtick = 10
+local stable = 0
 pfQuest.route:SetScript("OnUpdate", function()
   local xplayer, yplayer = GetPlayerMapPosition("player")
   local wrongmap = xplayer == 0 and yplayer == 0 and true or nil
-
-  -- always update arrow while distance is set
-  if wrongmap then
-    -- hide all arrow associated elements
-    this:SetAlpha(0)
-  elseif this.coords and this.coords[1] and this.coords[1][4] then
-    this:SetAlpha(1)
-    local xDelta = this.coords[1][1] - xplayer*100
-    local yDelta = this.coords[1][2] - yplayer*100
-    local dir = atan2(xDelta, -(yDelta))
-    dir = dir > 0 and (math.pi*2) - dir or -dir
-
-    local degtemp = dir
-    if degtemp < 0 then degtemp = degtemp + 360 end
-    local angle = math.rad(degtemp)
-    local player = pfQuestCompat.GetPlayerFacing()
-    angle = angle - player
-    local perc = math.abs(((math.pi - math.abs(angle)) / math.pi))
-    local r, g, b = pfUI.api.GetColorGradient(perc)
-    cell = modulo(floor(angle / (math.pi*2) * 108 + 0.5), 108)
-    local column = modulo(cell, 9)
-    local row = floor(cell / 9)
-    local xstart = (column * 56) / 512
-    local ystart = (row * 42) / 512
-    local xend = ((column + 1) * 56) / 512
-    local yend = ((row + 1) * 42) / 512
-
-    -- update arrow
-    this.arrow:SetTexCoord(xstart,xend,ystart,yend)
-    this.arrow:SetVertexColor(r,g,b)
-    this.distance:SetTextColor(r+.2,g+.2,b+.2)
-
-    if this.coords[1][3].texture then
-      this.texture:SetTexture(this.coords[1][3].texture)
-
-      local r, g, b = unpack(this.coords[1][3].vertex or {0,0,0})
-      if r > 0 or g > 0 or b > 0 then
-        this.texture:SetVertexColor(unpack(this.coords[1][3].vertex))
-      else
-        this.texture:SetVertexColor(1,1,1,1)
-      end
-    else
-      this.texture:SetTexture(pfQuestConfig.path.."\\img\\node")
-      this.texture:SetVertexColor(pfMap.str2rgb(this.coords[1][3].title))
-    end
-
-    -- update texture visibility
-    local alpha = this.coords[1][4] - 2
-    alpha = alpha > 1 and 1 or alpha
-    alpha = alpha < 0 and 0 or alpha
-    local texalpha = 1 - alpha
-    texalpha = texalpha > 1 and 1 or texalpha
-    texalpha = texalpha < 0 and 0 or texalpha
-
-    this.distance:SetAlpha(alpha)
-    this.arrow:SetAlpha(alpha)
-    this.arrow:Show()
-
-    this.texture:SetAlpha(texalpha)
-    this.texture:Show()
-  end
 
   -- limit distance and route updates to once per .1 seconds
   if ( this.tick or 5) > GetTime() then return else this.tick = GetTime() + .1 end
@@ -188,15 +108,20 @@ pfQuest.route:SetScript("OnUpdate", function()
   -- sort all coords by distance
   table.sort(this.coords, function(a,b) return a[4] < b[4] end)
 
-  -- abort without any nodes
-  if not this.coords[1] then
+  -- show arrow as soon as the route is stable enough
+  if this.coords[1] and this.coords[1][4] and stable > 3 then
+    this.arrow:Show()
+  end
+
+  -- abort without any nodes or distances
+  if not this.coords[1] or not this.coords[1][4] then
     ClearPath(objectivepath)
     ClearPath(playerpath)
     return
   end
 
-  -- check first node
-  if this.firstnode ~= tostring(this.coords[1][1]..this.coords[1][2]) or TODO_TABLE_CHANGED then
+  -- check first node for changes
+  if this.firstnode ~= tostring(this.coords[1][1]..this.coords[1][2]) then
     this.firstnode = tostring(this.coords[1][1]..this.coords[1][2])
 
     -- recalculate objective paths
@@ -212,72 +137,161 @@ pfQuest.route:SetScript("OnUpdate", function()
         DrawLine(objectivepath, route[i-1][1],route[i-1][2],route[i][1],route[i][2])
       end
     end
+
+    -- we can't trust the table yet
+    stable = 0
   end
+
+  if wrongmap then
+    -- hide player-to-object path
+    ClearPath(playerpath)
+  else
+    -- draw player-to-object path
+    ClearPath(playerpath)
+    DrawLine(playerpath,xplayer*100,yplayer*100,this.coords[1][1],this.coords[1][2],true)
+  end
+
+  -- increase stability counter
+  stable = stable + 1
+end)
+
+pfQuest.route.arrow = CreateFrame("Frame", "pfQuestRouteArrow", UIParent)
+pfQuest.route.arrow:SetPoint("CENTER", 0, -100)
+pfQuest.route.arrow:SetWidth(56)
+pfQuest.route.arrow:SetHeight(42)
+pfQuest.route.arrow:SetClampedToScreen(true)
+pfQuest.route.arrow:SetMovable(true)
+pfQuest.route.arrow:EnableMouse(true)
+pfQuest.route.arrow:RegisterForDrag('LeftButton')
+pfQuest.route.arrow:SetScript("OnDragStart", function()
+  if IsShiftKeyDown() then
+    this:StartMoving()
+  end
+end)
+
+pfQuest.route.arrow:SetScript("OnDragStop", function()
+  this:StopMovingOrSizing()
+end)
+
+local speed, lastdist, speedtick = 0, 0, 10
+pfQuest.route.arrow:SetScript("OnUpdate", function()
+  local xplayer, yplayer = GetPlayerMapPosition("player")
+  local wrongmap = xplayer == 0 and yplayer == 0 and true or nil
+  local target = this.parent.coords and this.parent.coords[1] and this.parent.coords[1][4] and this.parent.coords[1] or nil
+
+  -- disable arrow on invalid map/route
+  if not target or wrongmap then
+    this:Hide()
+    return
+  end
+
+  local xDelta = target[1] - xplayer*100
+  local yDelta = target[2] - yplayer*100
+  local dir = atan2(xDelta, -(yDelta))
+  dir = dir > 0 and (math.pi*2) - dir or -dir
+
+  local degtemp = dir
+  if degtemp < 0 then degtemp = degtemp + 360 end
+  local angle = math.rad(degtemp)
+  local player = pfQuestCompat.GetPlayerFacing()
+  angle = angle - player
+  local perc = math.abs(((math.pi - math.abs(angle)) / math.pi))
+  local r, g, b = pfUI.api.GetColorGradient(perc)
+  cell = modulo(floor(angle / (math.pi*2) * 108 + 0.5), 108)
+  local column = modulo(cell, 9)
+  local row = floor(cell / 9)
+  local xstart = (column * 56) / 512
+  local ystart = (row * 42) / 512
+  local xend = ((column + 1) * 56) / 512
+  local yend = ((row + 1) * 42) / 512
 
   -- calculate speed
   if speedtick > 0 then
     speedtick = speedtick - 1
   else
     speedtick = 10
-    speed = lastdist - this.coords[1][4]
-    lastdist = this.coords[1][4]
+    speed = lastdist - target[4]
+    lastdist = target[4]
   end
 
-  -- draw player
-  if wrongmap then
-    ClearPath(playerpath)
+  -- update arrow
+  this.model:SetTexCoord(xstart,xend,ystart,yend)
+  this.model:SetVertexColor(r,g,b)
+  this.distance:SetTextColor(r+.2,g+.2,b+.2)
+
+  if target[3].texture then
+    this.texture:SetTexture(target[3].texture)
+
+    local r, g, b = unpack(target[3].vertex or {0,0,0})
+    if r > 0 or g > 0 or b > 0 then
+      this.texture:SetVertexColor(unpack(target[3].vertex))
+    else
+      this.texture:SetVertexColor(1,1,1,1)
+    end
   else
-    ClearPath(playerpath)
-    DrawLine(playerpath,xplayer*100,yplayer*100,this.coords[1][1],this.coords[1][2],true)
+    this.texture:SetTexture(pfQuestConfig.path.."\\img\\node")
+    this.texture:SetVertexColor(pfMap.str2rgb(target[3].title))
   end
+
+  -- update texture visibility
+  local alpha = target[4] - 2
+  alpha = alpha > 1 and 1 or alpha
+  alpha = alpha < 0 and 0 or alpha
+  local texalpha = 1 - alpha
+  texalpha = texalpha > 1 and 1 or texalpha
+  texalpha = texalpha < 0 and 0 or texalpha
 
   -- set title text
   local color = "|cffffcc00"
-  if tonumber(this.coords[1][3]["qlvl"]) then
-    color = pfMap:HexDifficultyColor(tonumber(this.coords[1][3]["qlvl"]))
+  if tonumber(target[3]["qlvl"]) then
+    color = pfMap:HexDifficultyColor(tonumber(target[3]["qlvl"]))
   end
-  this.title:SetText(color..this.coords[1][3].title .. "|r")
+  this.title:SetText(color..target[3].title .. "|r")
 
   -- set description text
-  if this.coords[1][3].description then
-    this.description:SetText(this.coords[1][3].description)
+  if target[3].description then
+    this.description:SetText(target[3].description)
   else
     this.description:SetText("")
   end
 
   -- set distance
-  if this.coords[1][4] > 1 and speed > 0 and floor(this.coords[1][4]/speed) > 0 then
-    this.distance:SetText("|cffaaaaaa"..floor(this.coords[1][4]*10) .. " yards (|r" ..  SecondsToTime(floor(this.coords[1][4]/speed)) .. "|cffaaaaaa)")
+  if target[4] > 1 and speed > 0 and floor(target[4]/speed) > 0 then
+    this.distance:SetText("|cffaaaaaa"..floor(target[4]*10) .. " yards (|r" ..  SecondsToTime(floor(target[4]/speed)) .. "|cffaaaaaa)")
   else
-    this.distance:SetText("|cffaaaaaa"..floor(this.coords[1][4]*10) .. " yards")
+    this.distance:SetText("|cffaaaaaa"..floor(target[4]*10) .. " yards")
   end
+
+  this.distance:SetAlpha(alpha)
+  this.texture:SetAlpha(texalpha)
+  this.model:SetAlpha(alpha)
 end)
 
-pfQuest.route.texture = pfQuest.route:CreateTexture("pfQuestRouteNodeTexture", "OVERLAY")
-pfQuest.route.texture:SetWidth(32)
-pfQuest.route.texture:SetHeight(32)
-pfQuest.route.texture:SetPoint("BOTTOM", 0, 0)
-pfQuest.route.texture:Hide()
+pfQuest.route.arrow.texture = pfQuest.route.arrow:CreateTexture("pfQuestRouteNodeTexture", "OVERLAY")
+pfQuest.route.arrow.texture:SetWidth(32)
+pfQuest.route.arrow.texture:SetHeight(32)
+pfQuest.route.arrow.texture:SetPoint("BOTTOM", 0, 0)
 
-pfQuest.route.arrow = pfQuest.route:CreateTexture("pfQuestRouteArrow", "OVERLAY")
-pfQuest.route.arrow:SetTexture(pfQuestConfig.path.."\\img\\arrow")
-pfQuest.route.arrow:SetAllPoints()
-pfQuest.route.arrow:Hide()
+pfQuest.route.arrow.model = pfQuest.route.arrow:CreateTexture("pfQuestRouteArrow", "OVERLAY")
+pfQuest.route.arrow.model:SetTexture(pfQuestConfig.path.."\\img\\arrow")
+pfQuest.route.arrow.model:SetAllPoints()
 
-pfQuest.route.title = pfQuest.route:CreateFontString("pfQuestRouteText", "HIGH", "GameFontWhite")
-pfQuest.route.title:SetPoint("TOP", pfQuest.route.arrow, "BOTTOM", 0, -10)
-pfQuest.route.title:SetFont(pfUI.font_default, pfUI_config.global.font_size+1, "OUTLINE")
-pfQuest.route.title:SetTextColor(1,.8,.2)
-pfQuest.route.title:SetJustifyH("CENTER")
+pfQuest.route.arrow.title = pfQuest.route.arrow:CreateFontString("pfQuestRouteText", "HIGH", "GameFontWhite")
+pfQuest.route.arrow.title:SetPoint("TOP", pfQuest.route.arrow.model, "BOTTOM", 0, -10)
+pfQuest.route.arrow.title:SetFont(pfUI.font_default, pfUI_config.global.font_size+1, "OUTLINE")
+pfQuest.route.arrow.title:SetTextColor(1,.8,.2)
+pfQuest.route.arrow.title:SetJustifyH("CENTER")
 
-pfQuest.route.description = pfQuest.route:CreateFontString("pfQuestRouteText", "HIGH", "GameFontWhite")
-pfQuest.route.description:SetPoint("TOP", pfQuest.route.title, "BOTTOM", 0, -2)
-pfQuest.route.description:SetFont(pfUI.font_default, pfUI_config.global.font_size, "OUTLINE")
-pfQuest.route.description:SetTextColor(1,1,1)
-pfQuest.route.description:SetJustifyH("CENTER")
+pfQuest.route.arrow.description = pfQuest.route.arrow:CreateFontString("pfQuestRouteText", "HIGH", "GameFontWhite")
+pfQuest.route.arrow.description:SetPoint("TOP", pfQuest.route.arrow.title, "BOTTOM", 0, -2)
+pfQuest.route.arrow.description:SetFont(pfUI.font_default, pfUI_config.global.font_size, "OUTLINE")
+pfQuest.route.arrow.description:SetTextColor(1,1,1)
+pfQuest.route.arrow.description:SetJustifyH("CENTER")
 
-pfQuest.route.distance = pfQuest.route:CreateFontString("pfQuestRouteDistance", "HIGH", "GameFontWhite")
-pfQuest.route.distance:SetPoint("TOP", pfQuest.route.description, "BOTTOM", 0, -2)
-pfQuest.route.distance:SetFont(pfUI.font_default, pfUI_config.global.font_size-1, "OUTLINE")
-pfQuest.route.distance:SetTextColor(.8,.8,.8)
-pfQuest.route.distance:SetJustifyH("CENTER")
+pfQuest.route.arrow.distance = pfQuest.route.arrow:CreateFontString("pfQuestRouteDistance", "HIGH", "GameFontWhite")
+pfQuest.route.arrow.distance:SetPoint("TOP", pfQuest.route.arrow.description, "BOTTOM", 0, -2)
+pfQuest.route.arrow.distance:SetFont(pfUI.font_default, pfUI_config.global.font_size-1, "OUTLINE")
+pfQuest.route.arrow.distance:SetTextColor(.8,.8,.8)
+pfQuest.route.arrow.distance:SetJustifyH("CENTER")
+
+pfQuest.route.arrow.parent = pfQuest.route
