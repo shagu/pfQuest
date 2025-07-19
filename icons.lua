984b017df3f3dbf68817f784030d36e645a3d714
shagu <ericmauser@googlemail.com>
2025-07-19 09:55:27 +0200

    icons: add file to specify custom tracking icons

---
+++ icons.lua
@@ -0,0 +1,16 @@
+-- Custom Icons for Specific Nodes
+--
+-- This file contains custom icon definitions for specific nodes in the game.
+-- Each node has a unique identifier that is used to determine its localized name.
+-- Negative values are used to assign icons to game objects. Positive values are
+-- used to assign icons to units. To avoid duplicates, it's essential to use only
+-- one id per object or unit name.
+
+do -- /db track mines
+end
+
+do -- /db track herbs
+end
+
+do -- /db track chests
+end
